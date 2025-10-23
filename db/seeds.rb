2372d93bceff3ec0aae7aa257140a3f68e1c11cb
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2025-10-23 16:01:39 -0700

    Add seed data for Flights table

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,22 @@
 [ "TIA", "EZE", "AUA", "BNE", "CBR", "OOL", "DRW", "MEL", "GGT", "FLN" ].each do |code|
   Airport.create(code: code)
 end
+
+flight_seed = []
+10.times do |i|
+  departure_id = Random.rand(1..10)
+  arrival_id = Random.rand(1..10)
+  until arrival_id != departure_id
+    arrival_id = Random.rand(1..10)
+  end
+  flight_seed << {
+    start: DateTime.new(Time.now.year + 1, Random.rand(1..12), Random.rand(1..28), Random.rand(0..23), Random.rand(0..59)),
+    duration: Random.rand(1..19),
+    departure_airport_id: departure_id,
+    arrival_airport_id: arrival_id
+  }
+end
+
+flight_seed.each do |flight|
+  Flight.create(start: flight[:start], duration: flight[:duration], departure_airport_id: flight[:departure_airport_id], arrival_airport_id: flight[:arrival_airport_id])
+end
