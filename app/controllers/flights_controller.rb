5f052d8f78f13ab25acae640dea3bf7cea1696b6
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2025-10-24 15:34:16 -0700

    Add Flight controller and route, action for Index

---
+++ app/controllers/flights_controller.rb
@@ -0,0 +1,7 @@
+class FlightsController < ApplicationController
+  def index
+    @departure_airports = Airport.joins("INNER JOIN flights ON flights.departure_airport_id = airports.id").distinct.pluck(:code)
+    @arrival_airports = Airport.joins("INNER JOIN flights ON flights.arrival_airport_id = airports.id").distinct.pluck(:code)
+    @dates = Flight.select(:start).map { |date| [ date.start.to_date.to_fs(:long), date.start.to_date.to_fs(:number) ] }.uniq!
+  end
+end
