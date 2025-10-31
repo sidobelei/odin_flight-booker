d4e98a0d412db5da396bf237142da4e8fef220cf
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2025-10-31 15:25:59 -0700

    Edit order of airports listed on Home search form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,7 @@
 class FlightsController < ApplicationController
   def index
-    @departure_airports = Airport.joins("INNER JOIN flights ON flights.departure_airport_id = airports.id").distinct.pluck(:code)
-    @arrival_airports = Airport.joins("INNER JOIN flights ON flights.arrival_airport_id = airports.id").distinct.pluck(:code)
+    @departure_airports = Airport.joins("INNER JOIN flights ON flights.departure_airport_id = airports.id").order(:code).distinct.pluck(:code)
+    @arrival_airports = Airport.joins("INNER JOIN flights ON flights.arrival_airport_id = airports.id").order(:code).distinct.pluck(:code)
     date_list = Flight.select(:start).order(:start).map { |date| [ date.start.to_date.to_fs(:long), date.start.to_date.to_fs(:number) ] }
     @dates = date_list.uniq
 
