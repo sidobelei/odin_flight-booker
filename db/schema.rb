b517663d5f8566a7e96bab703d2b73c4a0a39ffc
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2025-11-04 11:35:16 -0800

    Remove passenger_id column from Bookings table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_31_224530) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_04_193335) do
   create_table "airports", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -20,10 +20,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_31_224530) do
   create_table "bookings", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "passenger_id", null: false
     t.integer "flight_id", null: false
     t.index ["flight_id"], name: "index_bookings_on_flight_id"
-    t.index ["passenger_id"], name: "index_bookings_on_passenger_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -45,7 +43,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_31_224530) do
   end
 
   add_foreign_key "bookings", "flights"
-  add_foreign_key "bookings", "passengers"
   add_foreign_key "flights", "airports", column: "arrival_airport_id"
   add_foreign_key "flights", "airports", column: "departure_airport_id"
 end
