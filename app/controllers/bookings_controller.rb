80651dccf17ff77b4c1c5a8c597b0ecd099e5ef5
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2025-11-06 13:25:10 -0800

    Add create booking form with passengers' info

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,29 @@
+class BookingsController < ApplicationController
+  def show
+  end
+
+  def new
+    @booking = Booking.new
+    @flight = Flight.select("flights.id AS id", "departure.code AS departure_code", "arrival.code AS arrival_code", "flights.start AS date")
+      .joins("INNER JOIN airports AS departure ON departure.id = flights.departure_airport_id")
+      .joins("INNER JOIN airports AS arrival ON arrival.id = flights.arrival_airport_id")
+      .where("flights.id = ?", params[:id])
+      .first
+
+    params[:num_tickets].to_i.times { @booking.passengers.build }
+  end
+
+  def create
+    @booking = Booking.create(booking_params)
+    if @booking.valid?
+      redirect_to @booking
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def booking_params
+      params.expect(booking: [ :flight_id, passengers_attributes: [ [ :name, :email ] ] ])
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "flights#index"
   resources :flights, only: [ :index ]
+  resources :bookings, only: [ :new, :create, :show ]
 end
